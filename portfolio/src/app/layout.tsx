62957479a33fab5af9e20ad5fb2aa31d89159d55
Ankit Kiran <95562947+Aankirz@users.noreply.github.com>
2026-07-04 22:24:26 +0530

    feat: Add Microsoft Clarity analytics script

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -128,6 +128,18 @@ export default function RootLayout({
           }}
         />
         <JsonLdScript data={getWebSiteJsonLd()} />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xh9xnjftz0");
+            `,
+          }}
+        />
       </head>
 
       <body>
